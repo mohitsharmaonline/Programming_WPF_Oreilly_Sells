a5d5b55e7219a028b4e3d33288486b0a7fcd6304
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-10-20 02:32:16 +0530

    adding databinding compatible class

---
+++ Chapter_001/DAtaBindingExample/NickName.cs
@@ -0,0 +1,53 @@
+﻿
+using System.ComponentModel;
+
+namespace DAtaBindingExample
+{
+    public class NickName : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string propName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(sender: this, e: new PropertyChangedEventArgs(propertyName: propName));
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                Notify(nameof(Name)); // Notify Consumers
+            }
+        }
+
+        private string nick;
+
+        public string Nick
+        {
+            get { return nick; }
+            set
+            {
+                nick = value;
+                Notify(nameof(Nick));   // Notify Consumers
+            }
+        }
+
+        public NickName()
+        {
+
+        }
+
+        public NickName(string name, string nick)
+        {
+            this.name = name;
+            this.nick = nick;
+        }
+
+    }
+}
