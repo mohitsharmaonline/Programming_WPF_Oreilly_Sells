25f00820acf9fb0a9eeae543f7cfe45bebab651e
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-10-20 02:45:08 +0530

    adding observable collection.

---
+++ Chapter_001/DAtaBindingExample/NickNames.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace DAtaBindingExample
+{
+
+    // INotifyCollectionChanged: Standard way of notifying consumers of collection when they change.
+    // ObservableCollection: implementation of INotifyCollectionChanged provided by WPF.
+    public class NickNames : ObservableCollection<NickName>
+    {
+    }
+}
