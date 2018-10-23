62878eed556178f4c938dc5bfb6b341a533cb554
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-10-24 02:15:54 +0530

    Introduction to resources and copied code from DataBindingDemo to modify it for resouce introduction.

---
+++ Chapter_001/Resources/DAtaBindingExample/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Windows;
+
+namespace DAtaBindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NickNames names;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.addButton.Click += AddButton_Click;
+
+            this.names = new NickNames();
+
+            // Make data available for binding
+            dockPanel.DataContext = this.names;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.names.Add(new NickName(name: textBoxName.Text, nick: textBoxNick.Text));
+        }
+    }
+}
